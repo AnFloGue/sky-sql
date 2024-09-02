947c3fbaeddbd141ab878b51cc6bd5b095ba155d
AnFlGue <mokshaflores@icloud.com>
2024-09-02 17:44:49 +0200

    01 Commit:

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
-import sqlite3
 
 QUERY_FLIGHT_BY_ID = """
 SELECT flights.*, airlines.AIRLINE as airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY
@@ -10,10 +9,10 @@ WHERE flights.ID = :id
 """
 
 QUERY_DELAYED_FLIGHTS_BY_AIRPORT = """
-SELECT flights.*, airlines.name as airline, flights.id as FLIGHT_ID, flights.departure_delay as DELAY
+SELECT flights.*, airlines.AIRLINE as airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY
 FROM flights
-JOIN airlines ON flights.airline_id = airlines.id
-WHERE flights.origin_airport = :airport AND flights.departure_delay >= 20
+JOIN airlines ON flights.AIRLINE = airlines.ID
+WHERE flights.ORIGIN_AIRPORT = :airport AND flights.DEPARTURE_DELAY >= 20
 """
 
 QUERY_FLIGHTS_BY_DATE = """
@@ -24,32 +23,17 @@ WHERE flights.YEAR = :year AND flights.MONTH = :month AND flights.DAY = :day
 """
 
 QUERY_DELAYED_FLIGHTS_BY_AIRLINE = """
-SELECT flights.*, airlines.name as airline, flights.id as FLIGHT_ID, flights.departure_delay as DELAY
+SELECT flights.*, airlines.AIRLINE as airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY
 FROM flights
-JOIN airlines ON flights.airline_id = airlines.id
-WHERE airlines.name = :airline AND flights.departure_delay >= 20
+JOIN airlines ON flights.AIRLINE = airlines.ID
+WHERE airlines.AIRLINE = :airline AND flights.DEPARTURE_DELAY >= 20
 """
 
 class FlightData:
-    """
-    The FlightData class is a Data Access Layer (DAL) object that provides an
-    interface to the flight data in the SQLITE database. When the object is created,
-    the class forms connection to the sqlite database file, which remains active
-    until the object is destroyed.
-    """
-
     def __init__(self, db_uri):
-        """
-        Initialize a new engine using the given database URI
-        """
         self._engine = create_engine(db_uri)
     
     def _execute_query(self, query, params):
-        """
-        Execute an SQL query with the params provided in a dictionary,
-        and returns a list of records (dictionary-like objects).
-        If an exception was raised, print the error, and return an empty list.
-        """
         try:
             with self._engine.connect() as connection:
                 result = connection.execute(text(query), params)
@@ -57,37 +41,22 @@ class FlightData:
         except sqlalchemy.exc.SQLAlchemyError as e:
             print(f"Error executing query: {e}")
             return []
+
     def get_flight_by_id(self, flight_id):
-        """
-        Searches for flight details using flight ID.
-        If the flight was found, returns a list with a single record.
-        """
         params = {'id': flight_id}
         return self._execute_query(QUERY_FLIGHT_BY_ID, params)
 
     def get_delayed_flights_by_airport(self, airport):
-        """
-        Searches for delayed flights by origin airport.
-        """
         params = {'airport': airport}
         return self._execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRPORT, params)
 
     def get_flights_by_date(self, day, month, year):
-        """
-        Searches for flights by date.
-        """
         params = {'day': day, 'month': month, 'year': year}
         return self._execute_query(QUERY_FLIGHTS_BY_DATE, params)
 
     def get_delayed_flights_by_airline(self, airline):
-        """
-        Searches for delayed flights by airline.
-        """
         params = {'airline': airline}
         return self._execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRLINE, params)
 
     def __del__(self):
-        """
-        Closes the connection to the database when the object is about to be destroyed
-        """
         self._engine.dispose()
--- main.py
+++ main.py
@@ -1,62 +1,36 @@
 import data
 from datetime import datetime
-import sqlalchemy
 
 SQLITE_URI = 'sqlite:///data/flights.sqlite3'
 IATA_LENGTH = 3
 
-
 def delayed_flights_by_airline(data_manager):
-    """
-    Asks the user for a textual airline name (any string will work here).
-    Then runs the query using the data object method "get_delayed_flights_by_airline".
-    When results are back, calls "print_results" to show them to on the screen.
-    """
     airline_input = input("Enter airline name: ")
     results = data_manager.get_delayed_flights_by_airline(airline_input)
     print_results(results)
 
-
 def delayed_flights_by_airport(data_manager):
-    """
-    Asks the user for a textual IATA 3-letter airport code (loops until input is valid).
-    Then runs the query using the data object method "get_delayed_flights_by_airport".
-    When results are back, calls "print_results" to show them to on the screen.
-    """
     valid = False
     while not valid:
         airport_input = input("Enter origin airport IATA code: ")
-        # Valide input
         if airport_input.isalpha() and len(airport_input) == IATA_LENGTH:
             valid = True
     results = data_manager.get_delayed_flights_by_airport(airport_input)
     print_results(results)
 
-
 def flight_by_id(data_manager):
-    """
-    Asks the user for a numeric flight ID,
-    Then runs the query using the data object method "get_flight_by_id".
-    When results are back, calls "print_results" to show them to on the screen.
-    """
     valid = False
     while not valid:
         try:
             id_input = int(input("Enter flight ID: "))
-        except Exception as e:
+        except Exception:
             print("Try again...")
         else:
             valid = True
     results = data_manager.get_flight_by_id(id_input)
     print_results(results)
 
-
 def flights_by_date(data_manager):
-    """
-    Asks the user for date input (and loops until it's valid),
-    Then runs the query using the data object method "get_flights_by_date".
-    When results are back, calls "print_results" to show them to on the screen.
-    """
     valid = False
     while not valid:
         try:
@@ -69,22 +43,14 @@ def flights_by_date(data_manager):
     results = data_manager.get_flights_by_date(date.day, date.month, date.year)
     print_results(results)
 
-
 def print_results(results):
-    """
-    Get a list of flight results (List of dictionary-like objects from SQLAlchemy).
-    Even if there is one result, it should be provided in a list.
-    Each object *has* to contain the columns:
-    FLIGHT_ID, ORIGIN_AIRPORT, DESTINATION_AIRPORT, AIRLINE, and DELAY.
-    """
     if results is None:
         results = []
 
     print(f"Got {len(results)} results.")
     for result in results:
-        # Check that all required columns are in place
         try:
-            delay = int(result['DELAY']) if result['DELAY'] else 0  # If delay columns is NULL, set it to 0
+            delay = int(result['DELAY']) if result['DELAY'] else 0
             origin = result['ORIGIN_AIRPORT']
             dest = result['DESTINATION_AIRPORT']
             airline = result['AIRLINE']
@@ -92,52 +58,42 @@ def print_results(results):
             print("Error showing results: ", e)
             return
 
-        # Different prints for delayed and non-delayed flights
         if delay and delay > 0:
             print(f"{result['ID']}. {origin} -> {dest} by {airline}, Delay: {delay} Minutes")
         else:
             print(f"{result['ID']}. {origin} -> {dest} by {airline}")
 
+def custom_exit(data_manager):
+    print("Goodbye, thanks for visiting!")
+    exit()
+
 def show_menu_and_get_input():
-    """
-    Show the menu and get user input.
-    If it's a valid option, return a pointer to the function to execute.
-    Otherwise, keep asking the user for input.
-    """
     print("Menu:")
     for key, value in FUNCTIONS.items():
         print(f"{key}. {value[1]}")
 
-    # Input loop
     while True:
         try:
             choice = int(input())
             if choice in FUNCTIONS:
                 return FUNCTIONS[choice][0]
-        except ValueError as e:
+        except ValueError:
             pass
         print("Try again...")
 
-"""
-Function Dispatch Dictionary
-"""
-FUNCTIONS = { 1: (flight_by_id, "Show flight by ID"),
-              2: (flights_by_date, "Show flights by date"),
-              3: (delayed_flights_by_airline, "Delayed flights by airline"),
-              4: (delayed_flights_by_airport, "Delayed flights by origin airport"),
-              5: (quit, "Exit")
-             }
-
+FUNCTIONS = {
+    1: (flight_by_id, "Show flight by ID"),
+    2: (flights_by_date, "Show flights by date"),
+    3: (delayed_flights_by_airline, "Delayed flights by airline"),
+    4: (delayed_flights_by_airport, "Delayed flights by origin airport"),
+    5: (custom_exit, "Exit")
+}
 
 def main():
-    # Create an instance of the Data Object using our SQLite URI
     data_manager = data.FlightData(SQLITE_URI)
-
-    # The Main Menu loop
     while True:
         choice_func = show_menu_and_get_input()
         choice_func(data_manager)
-
 
 if __name__ == "__main__":
     main()
